124cf227952655282ba131d95f260d46000ddfca
alamindesign <mail.muhammadit@gmail.com>
2022-11-17 16:30:08 +0600

    perseint function.

--- 1 javascritp class/js/index.js
+++ 1 javascritp class/js/index.js
@@ -15,3 +15,8 @@ alert("hello! this is "+$name);
 document.write("<p style='color: green;'>This is a paragraph form javascript.</p>");
 document.write("Hello");
 document.writeln("This is second para");
+let num=120;
+document.write(num.toString());
+console.log(num.toString());
+let hello='140';
+document.write(hello=parseInt(hello,7));
