2ab3fb95e1deafca6bb1d9174271a8d7ac61a51a
alamindesign <mail.muhammadit@gmail.com>
2022-11-20 15:33:05 +0600

    class on string

--- 1 javascritp class/js/index.js
+++ 1 javascritp class/js/index.js
@@ -1,38 +1,38 @@
 
-// let food ={
-//     rice:'steamed',
-//     potato: 'boiled',
-//     water: 10,
-//     Dal:3
-// }
-// food.water=5;
-// console.log(food.water);
-// let greeting='Hello ';
-// let $name='Al Amin';
-// console.log(greeting+$name);
-// document.write(greeting+$name);
-// alert("hello! this is "+$name);
-// document.write("<p style='color: green;'>This is a paragraph form javascript.</p>");
-// document.write("Hello");
-// document.writeln("This is second para");
-// let num=120;
-// document.write(num.toString());
-// console.log(num.toString());
-// let hello='140';
-// document.write(hello=parseInt(hello,7));
-// function mistake(mName){
-//     console.log("You have pressed wrong key "+mName)
-// }              
-// mistake('Nayma');
-// mistake('Al Amin');
-// function addMath(n,n1){
-//     console.log(n+n1);
-// }
-// addMath(2,5);
+let food ={
+    rice:'steamed',
+    potato: 'boiled',
+    water: 10,
+    Dal:3
+}
+food.water=5;
+console.log(food.water);
+let greeting='Hello ';
+let $name='Al Amin';
+console.log(greeting+$name);
+document.write(greeting+$name+"<br/>");
+alert("hello! this is "+$name);
+document.write("<p style='color: green;'>This is a paragraph form javascript.</p>");
+document.write("Hello"+"<br/>");
+document.writeln("This is second para"+"<br/>");
+let num=120;
+document.write(num.toString());
+console.log(num.toString());
+let hello='140';
+document.write(hello=parseInt(hello,7));
+function mistake(mName){
+    console.log("You have pressed wrong key "+mName)
+}              
+mistake('Nayma');
+mistake('Al Amin');
+function addMath(n,n1){
+    console.log(n+n1);
+}
+addMath(2,5);
 
-// let firstName=prompt("Enter Your fisrt Name: ");
-// let lastName=prompt("Enter Your last name: ");
-// document.write("Welcome!!"+" "+firstName+" "+lastName);
+let firstName=prompt("Enter Your fisrt Name: ");
+let lastName=prompt("Enter Your last name: ");
+document.write("Welcome!!"+" "+firstName+" "+lastName+"<br/>");
 
 // let firstNum=prompt("Enter the first Number");
 // let secondNum=prompt("Enter the second number");
@@ -42,6 +42,14 @@
 let firstNum=Number(prompt("Enter the first Number"));
 let secondNum=Number(prompt("Enter the second number"));
 let sum=firstNum+secondNum;
-document.write("The sum of two number is: "+sum);
+document.write("The sum of two number is: "+sum+"<br/>");
 
-
+document.write(Number(prompt("Enter first number"))+Number(prompt("Enter second number"))+"<br/>");
+let firstText=prompt("Enter the first text");
+let secondText=prompt("Enter the seconed text:");
+document.write(firstText.concat(secondText)+"<br/>");
+document.write(secondText.length);
+let lastname='Al Amin';
+document.write(lastname.slice(0,2));
+document.write("<br/>");
+document.write(lastname.charAt(3));
