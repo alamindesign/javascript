e9ddc673fab1cd8afa6d115986a9e2bb1bba16d1
alamindesign <mail.muhammadit@gmail.com>
2022-11-27 15:49:02 +0600

    loop

--- 1 javascritp class/js/index.js
+++ 1 javascritp class/js/index.js
@@ -132,3 +132,8 @@ switch(char) {
     default: 
         document.write("<br/> You didn't press A or B or C. So >>> Invalid");
 }
+
+var i=1;
+for(i=1;i<=10;i++){
+    document.write("<br/>"+i);
+}
