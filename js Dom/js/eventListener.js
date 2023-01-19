d4191e5c937e1f4aa43d7df4f8bef644df286dd3
alamindesign <mail.muhammadit@gmail.com>
2023-01-19 10:18:05 +0600

    querySelectorAll

--- js Dom/js/eventListener.js
+++ js Dom/js/eventListener.js
@@ -3,3 +3,15 @@ btn.addEventListener("click", giveAlert);
 function giveAlert(){
     alert("I'm working....");
 }
+let text = document.querySelector("#text");
+let btns = document.querySelectorAll(".myButton");
+btns[0].addEventListener("click", function(){
+    text.innerHTML = this.innerHTML + " Is clicked";
+})
+btns[1].addEventListener("click", function(){
+    text.innerHTML = this.innerHTML + " Is clicked";
+})
+btns[2].addEventListener("click", function(){
+    text.innerHTML = this.innerHTML + " Is clicked";
+})
+
