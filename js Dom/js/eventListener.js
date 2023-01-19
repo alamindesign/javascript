b6a22d9dd1759b6fc6f937f6c658f869b65a305f
alamindesign <mail.muhammadit@gmail.com>
2023-01-19 11:29:47 +0600

    consept of massenger

--- js Dom/js/eventListener.js
+++ js Dom/js/eventListener.js
@@ -1,17 +1,41 @@
 let btn = document.querySelector("button");
+let text = document.querySelector("#text");
+let btns = document.querySelectorAll(".myButton");
+let ul = document.querySelector("ul");
+let input = document.querySelector("#userinput");
+let send = document.querySelector("#send");
 btn.addEventListener("click", giveAlert);
 function giveAlert(){
     alert("I'm working....");
 }
-let text = document.querySelector("#text");
-let btns = document.querySelectorAll(".myButton");
-btns[0].addEventListener("click", function(){
-    text.innerHTML = this.innerHTML + " Is clicked";
-})
-btns[1].addEventListener("click", function(){
-    text.innerHTML = this.innerHTML + " Is clicked";
-})
-btns[2].addEventListener("click", function(){
-    text.innerHTML = this.innerHTML + " Is clicked";
-})
 
+// btns[0].addEventListener("click", function(){
+//     text.innerHTML = this.innerHTML + " Is clicked";
+// })
+// btns[1].addEventListener("click", function(){
+//     text.innerHTML = this.innerHTML + " Is clicked";
+// })
+// btns[2].addEventListener("click", function(){
+//     text.innerHTML = this.innerHTML + " Is clicked";
+// })
+for(var i = 0; i < btns.length ; i++){
+    btns[i].addEventListener("click", function(){
+            text.innerHTML = this.innerHTML + " Is clicked";
+    });
+}
+
+send.addEventListener("click", function(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+})
+input.addEventListener("keypress", function(event){
+    if(event.keyCode === 13){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+    }
+    
+})
